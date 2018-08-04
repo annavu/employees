378a2f092d16ae7ee6afe1fd9b827625c6f63793
annavu <anna.wrzos@gmail.com>
2018-08-04 17:59:02 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Employees exercise
+
+There are employee’s personal data in data.json file,
+- present them in a table 
+- implement filtering according to each attribute and sorting
+
+(build in progress)
---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+document.addEventListener("DOMContentLoaded", loadData);
+
+function load() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'data.json');
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      console.log(this.responseText);
+      console.log(JSON.parse(this.responseText));
+
+      const data = JSON.parse(this.responseText);
+
+      const ul = document.createElement('ul');
+      document.body.appendChild(ul);
+      
+      data.forEach(function(item) {
+        const li = document.createElement('li');
+        li.textContent = `${item.firstName} ${item.lastName}`
+        ul.appendChild(li);
+      })
+    }
+  }
+
+  xhr.send()
+}
+
+function loadData() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://api.myjson.com/bins/18ia4g');
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      
+      console.log(JSON.parse(this.responseText));
+
+      const data = JSON.parse(this.responseText);
+
+      const ul = document.createElement('ul');
+      document.body.appendChild(ul);
+      
+      data.forEach(function(item) {
+        const li = document.createElement('li');
+        li.textContent = `${item.firstName} ${item.lastName}`
+        ul.appendChild(li);
+      })
+    }
+  }
+
+  xhr.send()
+}
