9e0feb4576d1cd79afd52b21b44cf77c0b11fcfb
annavu <anna.wrzos@gmail.com>
2018-08-04 19:34:30 +0200

    update main.js with new classes

--- js/main.js
+++ js/main.js
@@ -1,54 +1,64 @@
 
-document.addEventListener("DOMContentLoaded", loadData);
+document.addEventListener("DOMContentLoaded", loadEmployees);
 
-function load() {
-  const xhr = new XMLHttpRequest();
+// function load() {
+//   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'data.json');
+//   xhr.open('GET', 'data.json');
 
-  xhr.onload = function() {
-    if(this.status === 200) {
-      console.log(this.responseText);
-      console.log(JSON.parse(this.responseText));
+//   xhr.onload = function() {
+//     if(this.status === 200) {
+//       console.log(this.responseText);
+//       console.log(JSON.parse(this.responseText));
 
-      const data = JSON.parse(this.responseText);
+//       const data = JSON.parse(this.responseText);
 
-      const ul = document.createElement('ul');
-      document.body.appendChild(ul);
+//       const ul = document.createElement('ul');
+//       document.body.appendChild(ul);
       
-      data.forEach(function(item) {
-        const li = document.createElement('li');
-        li.textContent = `${item.firstName} ${item.lastName}`
-        ul.appendChild(li);
-      })
-    }
-  }
+//       data.forEach(function(item) {
+//         const li = document.createElement('li');
+//         li.textContent = `${item.firstName} ${item.lastName}`
+//         ul.appendChild(li);
+//       })
+//     }
+//   }
 
-  xhr.send()
-}
+//   xhr.send()
+// }
 
-function loadData() {
-  const xhr = new XMLHttpRequest();
+// function loadData() {
+//   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'https://api.myjson.com/bins/18ia4g');
+//   xhr.open('GET', 'https://api.myjson.com/bins/18ia4g');
 
-  xhr.onload = function() {
-    if(this.status === 200) {
+//   xhr.onload = function() {
+//     if(this.status === 200) {
       
-      console.log(JSON.parse(this.responseText));
+//       console.log(JSON.parse(this.responseText));
 
-      const data = JSON.parse(this.responseText);
+//       const data = JSON.parse(this.responseText);
 
-      const ul = document.createElement('ul');
-      document.body.appendChild(ul);
+//       const ul = document.createElement('ul');
+//       document.body.appendChild(ul);
       
-      data.forEach(function(item) {
-        const li = document.createElement('li');
-        li.textContent = `${item.firstName} ${item.lastName}`
-        ul.appendChild(li);
-      })
-    }
-  }
+//       data.forEach(function(item) {
+//         const li = document.createElement('li');
+//         li.textContent = `${item.firstName} ${item.lastName}`
+//         ul.appendChild(li);
+//       })
+//     }
+//   }
 
-  xhr.send()
+//   xhr.send()
+// }
+
+const employees = new Employees();
+const ui = new UI();
+
+function loadEmployees() {
+  employees.getEmployees().then(function(response) {
+    console.log(response)
+    ui.show(response);
+  })
 }
